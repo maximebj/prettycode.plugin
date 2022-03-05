ada2f01cdafeaa14fa73c87534eb943afa9e3b44
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2022-03-05 11:37:27 +0100

    Template vars escaping

--- templates/code.php
+++ templates/code.php
@@ -1,24 +1,24 @@
-<div class="wp-block-prettycode-code <?php echo $align; ?> <?php echo $customClass; ?>">
+<div class="wp-block-prettycode-code <?php echo esc_attr( $align ); ?> <?php echo esc_attr( $customClass ); ?>">
   <header class="prettycode-header">
-    <div class="prettycode-lang is-lang-<?php echo $lang_slug; ?>">
-      <?php echo $lang_label; ?>
+    <div class="prettycode-lang is-lang-<?php echo esc_attr( $lang_slug ); ?>">
+      <?php echo esc_attr( $lang_label ); ?>
     </div>
     <div class="prettycode-file">
-      <?php echo $file; ?>
+      <?php echo esc_html( $file ); ?>
     </div>
   </header>
   <textarea
     class="prettycode-source"
-    name="codemirror-<?php echo $uid; ?>"
-    id="codemirror-<?php echo $uid; ?>"
+    name="codemirror-<?php echo esc_attr( $uid ); ?>"
+    id="codemirror-<?php echo esc_attr( $uid ); ?>"
   ><?php echo esc_html( $source ); ?></textarea>
   <script>
-    CodeMirror.fromTextArea( document.getElementById('codemirror-<?php echo $uid; ?>'), {
-      mode: '<?php echo $lang_mime ?: $lang_mode; ?>',
+    CodeMirror.fromTextArea( document.getElementById('codemirror-<?php echo esc_attr( $uid ); ?>'), {
+      mode: '<?php echo esc_attr( $lang_mime ?: $lang_mode ); ?>',
       readOnly: true,
-      theme: '<?php echo $theme; ?>',
+      theme: '<?php echo esc_attr( $theme ); ?>',
       lineNumbers: <?php echo ( $showLines ) ? 'true' : 'false'; ?>,
-      firstLineNumber: <?php echo $startLine; ?>,
+      firstLineNumber: <?php echo esc_attr( $startLine ); ?>,
       matchBrackets: true,
       indentUnit: 4,
       tabSize: 4,
